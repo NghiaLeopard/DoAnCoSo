83c9fd5ffd18e5f853ae272ebe1aa2594457f368
HgTrong <124127247+HgTrong@users.noreply.github.com>
2023-11-09 15:49:36 +0700

    Add files via upload

--- project.js
+++ project.js
@@ -1,51 +1,49 @@
-var widthfashion = document.getElementsByClassName('product-item')[1].offsetWidth + 30;
-var fashionPSlide = document.getElementById('product-slider');
-var watchedPSlide = document.getElementById('product-slider1');
-var fashionPPosition = 0;
-var watchedPPosition = 0;
-var minfashionWidth = 0;
-var minWatchedWidth = 0;
-var maxfashionWidth = document.getElementById('product-slider').children.length * widthfashion - widthfashion * 5;
-var maxWatchedWidth = document.getElementById('product-slider1').children.length * widthfashion - widthfashion * 5;
-function right() {
-  if (fashionPPosition > -maxfashionWidth) {
-    widthfashion = document.getElementsByClassName('product-item')[1].offsetWidth + 30;
-    fashionPPosition -= widthfashion;
-    fashionPSlide.style.transform = 'translateX(' + fashionPPosition + 'px)';
-  }
+var widthLaptop = document.getElementsByClassName('product-item')[1].offsetWidth+30;
+var laptopPSlide = document.getElementById('product-slider');
+var phonePSlide = document.getElementById('product-slider1');
+var laptopPPosition = 0;
+var phonePPosition = 0;
+var minLaptopWidth=0;
+var minPhoneWidth=0;
+var maxLaptopWidth=document.getElementById('product-slider').children.length * widthLaptop - (widthLaptop*5)
+var maxPhoneWidth=document.getElementById('product-slider1').children.length * widthLaptop - (widthLaptop*5)
+function right(){
+    if(laptopPPosition>-maxLaptopWidth){
+        widthlaptop = document.getElementsByClassName('product-item')[1].offsetWidth+30;
+        laptopPPosition-=widthLaptop;
+        laptopPSlide.style.transform="translateX("+laptopPPosition+"px)";
+    }
 }
-function right1() {
-  if (watchedPPosition > -maxWatchedWidth) {
-    widthfashion = document.getElementsByClassName('product-item')[1].offsetWidth + 30;
-    watchedPPosition -= widthfashion;
-    watchedPSlide.style.transform = 'translateX(' + watchedPPosition + 'px)';
-  }
+function right1(){
+    if(phonePPosition>-maxPhoneWidth){
+        widthLaptop = document.getElementsByClassName('product-item')[1].offsetWidth+30;
+        phonePPosition-=widthLaptop;
+        phonePSlide.style.transform="translateX("+phonePPosition+"px)";
+    }
 }
-function left() {
-  if (fashionPPosition < minfashionWidth) {
-    widthfashion = document.getElementsByClassName('product-item')[1].offsetWidth + 30;
-    fashionPPosition += widthfashion;
-    fashionPSlide.style.transform = 'translateX(' + fashionPPosition + 'px)';
-  }
+function left(){
+    if(laptopPPosition<minLaptopWidth){
+        widthLaptop = document.getElementsByClassName('product-item')[1].offsetWidth+30;
+        laptopPPosition+=widthLaptop;
+        laptopPSlide.style.transform="translateX("+laptopPPosition+"px)";
+    }
 }
-function left1() {
-  if (watchedPPosition < minWatchedWidth) {
-    widthfashion = document.getElementsByClassName('product-item')[1].offsetWidth + 30;
-    watchedPPosition += widthfashion;
-    watchedPSlide.style.transform = 'translateX(' + watchedPPosition + 'px)';
-  }
+function left1(){
+    if(phonePPosition<minPhoneWidth){
+        widthLaptop = document.getElementsByClassName('product-item')[1].offsetWidth+30;
+        phonePPosition+=widthLaptop;
+        phonePSlide.style.transform="translateX("+phonePPosition+"px)";
+    }
 }
-function chooseColor(e) {
-  var className = e.className;
-  var number = className.charAt(className.length - 1);
-  var openBox = 'box-img' + number;
-  var openbox_img = $(e)
-    .parent()
-    .parent()
-    .siblings('.' + openBox)[0];
-  var box_img = $(e).parent().parent().siblings('.box-img');
-  for (var i = 0; i < box_img.length; i++) {
-    box_img[i].style.display = 'none';
-  }
-  openbox_img.style.display = 'block';
+function chooseColor(e){
+    var className = e.className;
+    var number = className.charAt(className.length-1);
+    var openBox = "box-img"+number;
+    var openbox_img = $(e).parent().parent().siblings('.'+openBox)[0];
+    var box_img = $(e).parent().parent().siblings('.box-img');
+    for( var i =0;i<box_img.length;i++){
+        box_img[i].style.display='none';
+    }
+    openbox_img.style.display='block';
+
 }
