32feba1ea5c79db6888e05ff6933f747a455719e
tuananh212 <128112753+tuananhhh21@users.noreply.github.com>
2023-11-14 11:10:33 +0700

    update order api

---
+++ src/api/orderApi.js
@@ -0,0 +1,43 @@
+import axiosClient from './axiosClient';
+
+const orderApi = {
+  async getAll(params) {
+    // Transform _page to _start
+    const newParams = { ...params };
+    newParams._start = !params._page || params._page <= 1 ? 0 : (params._page - 1) * (params._limit || 50);
+    // Remove un-needed key
+    delete newParams._page;
+    // Fetch product list + count
+    const productList = await axiosClient.get('/products', { params: newParams });
+    //  Api don't support , should must call api count for pagination
+    const count = await axiosClient.get('/products/count', { params: newParams });
+    // Build response and return
+    return {
+      data: productList,
+      pagination: {
+        page: params._page,
+        limit: params._limit,
+        total: count,
+      },
+    };
+  },
+  get(id) {
+    const url = `/products/${id}`;
+    return axiosClient.get(url);
+  },
+
+  add(data) {
+    const url = `/orders`;
+    return axiosClient.post(url, data);
+  },
+  update(data) {
+    const url = `/products/${data.id}`;
+    return axiosClient.patch(url);
+  },
+  remove(id) {
+    const url = `/products/${id}`;
+    return axiosClient.delete(url);
+  },
+};
+
+export default productApi;
